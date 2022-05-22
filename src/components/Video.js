940e355795331d2c22877010594f79103ea60e9b
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-05-22 15:09:34 +0300

    React Player

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,50 +4,179 @@ const Video = () => {
   const data = [
     {
       title: "React hooks",
-      url: "",
+      url: "https://www.youtube.com/watch?v=hQAHSlTtcmY",
       name: "David Jonson",
-      photo: "",
+      photo:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       date: "20 Jun 2021",
       id: "1",
     },
     {
       title: "React hooks",
-      url: "",
+      url: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
       name: "David Jonson",
-      photo: "",
+      photo:
+        "https://images.pexels.com/photos/3756862/pexels-photo-3756862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       date: "20 Jun 2021",
       id: "2",
     },
     {
       title: "React hooks",
-      url: "",
+      url: "https://www.youtube.com/watch?v=Dorf8i6lCuk",
       name: "David Jonson",
-      photo: "",
+      photo:
+        "https://images.pexels.com/photos/1839904/pexels-photo-1839904.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
       date: "20 Jun 2021",
       id: "3",
     },
     {
       title: "React hooks",
-      url: "",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
       name: "David Jonson",
-      photo: "",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      date: "20 Jun 2021",
+      id: "4",
+    },
+    {
+      title: "React hooks",
+      url: "https://www.youtube.com/watch?v=LlvBzyy-558",
+      name: "David Jonson",
+      photo:
+        "https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       date: "20 Jun 2021",
       id: "4",
     },
   ];
 
   const [videData, setVideoData] = useState(data);
+  const [showMore, setShowMore] = useState(6);
+
+  const loadMore = () => {
+    setShowMore((prev) => prev + 3);
+  };
 
   return (
     <div className="container" style={{ marginTop: "80px" }}>
       <div className="row">
         {videData &&
-          videData.map((video, index) => (
-            <div className="col-xl-3 col-lg-4 col-md-4 col-sm-12">
-             <VideoDetails key={video.id} {...video}/>
+          videData.slice(0, showMore).map((video, index) => (
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+              <VideoDetails key={video.id} {...video} />
             </div>
           ))}
       </div>
+
+      {showMore >= data.length ? null : (
+            <div
+            onClick={loadMore}
+            className="loadmore"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontSize: "20px",
+              color: "white",
+              cursor: "pointer",
+              background: "tomato",
+              marginBottom:"40px",
+              borderRadius:"20px"
+            }}
+          >
+            Load More
+          </div>
+        )}
+    
     </div>
   );
 };
--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./video.css";
-
-const VideoDetails = ({title,name,url,photo,date}) => {
+import ReactPlayer from "react-player";
+const VideoDetails = ({ title, name, url, photo, date }) => {
   return (
     <React.Fragment>
-   
       <div className="video_item_design">
-      <p>{date}</p>
-        <h6>{title}</h6>
-        <p>{name}</p>
+        <ReactPlayer
+          loop={true}
+          playing={false}
+          controls
+          height="250px"
+          width="auto"
+          url={url}
+        />
+        <div className="video_title_design">
+          <h6>{title}</h6>
+          <p>{date}</p>
+          <div className="profile_pic_name">
+            <div className="profile_pic">
+              <img src={photo} alt="photo" />
+            </div>
+            <div className="name">
+             
+              <p>{name}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );

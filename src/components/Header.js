6aa333f0b4443f9eea85c373f95510ce6aff83a8
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-09 21:18:36 +0300

    Changing hover color

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <React.Fragment>
       <div className="container-fluid header-section">
           <div className="container header-title">
-            <h6>Video Player Web App</h6>
+            <h4>Video Player Web App</h4>
           </div>
       </div>
     </React.Fragment>
